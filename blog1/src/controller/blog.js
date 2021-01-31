6aa30b0ca86135eed78b1dfd092326fb7a432049
BBiiaoao <511299371@qq.com>
2021-02-01 00:04:49 +0800

    update:blog router

---
+++ blog1/src/controller/blog.js
@@ -0,0 +1,23 @@
+const getList = (author, kerword) => {
+    //先返回假数据（格式是正确的）
+    return [
+        {
+            id: 1,
+            title: '标题A',
+            content: '内容A',
+            createTime: 1612103627787,
+            author: 'zhangsan'
+        },
+        {
+            id: 2,
+            title: '标题B',
+            content: '内容B',
+            createTime: 1612103670730,
+            author: 'lisi'
+        }
+    ]
+}
+
+module.exports = {
+    getList,
+}
