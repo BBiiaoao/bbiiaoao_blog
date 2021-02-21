dfc1560f6dab775dca7c38ab10f39eb0f51d8e28
BBiiaoao <511299371@qq.com>
2021-02-21 22:48:33 +0800

    add:mysql

--- blog1/src/controller/blog.js
+++ blog1/src/controller/blog.js
@@ -1,21 +1,17 @@
+const { exec } = require('../db/mysql');
+
 const getList = (author, kerword) => {
-    //先返回假数据（格式是正确的）
-    return [
-        {
-            id: 1,
-            title: '标题A',
-            content: '内容A',
-            createTime: 1612103627787,
-            author: 'zhangsan'
-        },
-        {
-            id: 2,
-            title: '标题B',
-            content: '内容B',
-            createTime: 1612103670730,
-            author: 'lisi'
-        }
-    ]
+    let sql = `select * from blogs where 1=1 `;
+    if (author) {
+        sql += `and author='${author}' `;
+    }
+    if (kerword) {
+        sql += `and title like '%${keyword}%' `;
+    }
+    sql += `order by createtime desc;`
+
+    //返回 promise
+    return exec(sql);
 }
 
 const getDetail = (id) => {
